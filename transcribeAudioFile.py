536b3c474c87cbf509cb7d5a9d303db66c090ddb
ehanson8 <ehanson8@jh.edu>
2017-11-22 11:36:18 -0500

    updates

---
+++ transcribeAudioFile.py
@@ -0,0 +1,60 @@
+import json
+import time
+import requests
+from pydub import AudioSegment
+import os
+import secrets
+
+startTime = time.time()
+
+witKey = secrets.witKey
+filePath = secrets.filePath
+
+file = raw_input('Enter file name (including \'.mp3\')')
+fullFilePath = filePath+file
+
+sound = AudioSegment.from_mp3(fullFilePath)
+f = open(file+'.txt', 'wb')
+for i in range(1, 31):
+    sleepTime = 5
+    print 'waiting '+str(sleepTime)+' seconds'
+    time.sleep(sleepTime)
+    segmentStartTime = time.time()
+    split = len(sound) / 30
+    splitStartPoint = split * (i - 1)
+    splitEndPoint = split * i
+    if splitEndPoint > len(sound):
+        splitEndPoint = len(sound)
+
+    fileSegment = sound[splitStartPoint:splitEndPoint]
+    fileName = file.replace('.','_'+str(i)+'.')
+    fileSegment.export(filePath+fileName, format="mp3")
+    fullFilePath = filePath+fileName
+
+    soundFile = open(fullFilePath, 'rb')
+    url = "https://api.wit.ai/speech?v=20160526"
+    headers = {'Authorization': 'Bearer '+witKey, 'Content-Type': 'audio/mpeg3'}
+    print fileName
+    results = requests.post(url, data=soundFile, headers=headers).json()
+    while '_text' not in results:
+        print json.dumps(results)
+        sleepTime = 5
+        print 'waiting '+str(sleepTime)+' seconds'
+        time.sleep(sleepTime)
+        soundFile = open(fullFilePath, 'rb')
+        results = requests.post(url, data=soundFile, headers=headers).json()
+    results = results['_text']
+    f = open(file+'.txt', 'a')
+    f.write(' '+results)
+    print 'Segment transcript created'
+    os.remove(fullFilePath)
+
+    elapsedTime = time.time() - segmentStartTime
+    m, s = divmod(elapsedTime, 60)
+    h, m = divmod(m, 60)
+    print 'Segment processing time: ', '%d:%02d:%02d' % (h, m, s)
+
+elapsedTime = time.time() - startTime
+m, s = divmod(elapsedTime, 60)
+h, m = divmod(m, 60)
+print 'Total script run time: ', '%d:%02d:%02d' % (h, m, s)
