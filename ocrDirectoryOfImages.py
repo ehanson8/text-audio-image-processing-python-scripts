4ce4b8391714096fb739e94f6ebb8bc9b0f09b7a
ehanson8 <ehanson8@jhu.edu>
2018-07-03 15:41:28 -0400

    updates

--- ocrDirectoryOfImages.py
+++ ocrDirectoryOfImages.py
@@ -1,23 +1,29 @@
 import tesserocr
 import os
 import time
+import pytesseract
+from PIL import Image
 
 startTime = time.time()
 filePath = raw_input('Enter file path (e.g. \'C:/sampleImages/\'): ')
 files = os.listdir(filePath)
 for file in files:
-    print file
     if file.endswith('jpg'):
         print file
         fileName = file[:file.rindex('.')]
         imageFile = os.path.join(filePath, file)
-        print imageFile
+
+        #first OCR method
         text = tesserocr.file_to_text(imageFile)
+
+        ##second OCR method
+        #text = pytesseract.image_to_string(Image.open(imageFile))
+
         f=(open(filePath+fileName+'.txt', 'wb'))
         f.write(text.encode('utf-8'))
         print 'text file created'
     else:
-        print 'skipped'
+        print file, ' - skipped'
 
 elapsedTime = time.time() - startTime
 m, s = divmod(elapsedTime, 60)
