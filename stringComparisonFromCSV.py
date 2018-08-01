667de05607bfd4e7b530e1bea282e4fb23fb0f3f
ehanson8 <ehanson8@jhu.edu>
2018-08-01 14:11:11 -0400

    updates

--- stringComparisonFromCSV.py
+++ stringComparisonFromCSV.py
@@ -24,7 +24,7 @@ with open(fileName) as csvfile:
     for row in reader:
         nameList.append(str(row['name']))
 
-f=csv.writer(open('nameNearMatches.csv','wb'))
+f=csv.writer(open('stringNearMatches.csv','wb'))
 f.writerow(['percentage']+['name1']+['name2'])
 completeNearMatches = []
 for name in nameList:
